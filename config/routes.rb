f79affd8ba4df82615569c0d5d80dcc775183702
AnaCarrizales <acarrizales2@ucol.mx>
2024-01-25 19:23:26 -0600

    Resolve some observations
    Co-authored-by: Ramon Diaz <jramondiaz96@gmail.com>
    Co-authored-by: Yhoselin Sánchez <yossi2075.ys@gmail.com>
    Co-authored-by: Ana Landin <alandin1@ucol.mx>

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-
-  #resources: es un método utilizado para declarar rutas RESTful (get,post,put,delete,update,etc)}
-  #generará automáticamente rutas y acciones para un controlador específico, en este caso event_controller
   resources :event
   
-  #root: se usa para definir el index de la aplicación
   root 'home#index'
- 
-  # Asi se definen rutas personalizadas
-  #get 'Cualquier-cosa', to: 'pages#about'
-
-  #Asi se define una ruta simple
-  #get 'pages/about'
   
 end
