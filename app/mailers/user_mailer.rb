14b9640a4ef511f88543658c920daad8763b0147
AnaCarrizales <acarrizales2@ucol.mx>
2024-02-19 18:44:35 -0600

    Changed password_instructions
    Co-authored-by: Ramon Diaz <jramondiaz96@gmail.com>
    Co-authored-by: Yhoselin Sánchez <yossi2075.ys@gmail.com>
    Co-authored-by: Ana Landin <alandin1@ucol.mx>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,11 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Verifica tu correo electrónico')
   end
 
-   def reset_password_email(user)
+  def reset_password_email(user)
     @user = user
     @url  = edit_user_password_url(@user.reset_password_token)
-    mail(to: @user.email, subject: 'Cambia tu contraseña')
+    mail(to: @user.email, subject: 'Cambia tu contraseña') do |format|
+      format.html { render 'reset_password_email' }
+    end
   end
 end
